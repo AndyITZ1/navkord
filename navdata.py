a4337a438c67f603141a12b832f35ae936bbc051
Andytr13 <44204082+Andytr13@users.noreply.github.com>
2021-10-20 23:56:36 -0400

    Update Bot Files

---
+++ navdata.py
@@ -0,0 +1,11 @@
+from pymongo import MongoClient
+import os
+from dotenv import load_dotenv
+
+load_dotenv('know.env')
+
+conn_str = os.getenv('MONGODB_URL')
+
+client = MongoClient(conn_str, serverSelectionTimeoutMS=5000)
+
+mydb = MyClient["users"]
